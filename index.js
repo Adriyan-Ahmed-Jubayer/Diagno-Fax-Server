7d56159965c36274ff0d774c3220e65cb923515e
Adriyan-Ahmed-Jubayer <adriyan.khan.jubayer@gmail.com>
2023-11-27 03:21:56 +0600

    Making api to get single data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,7 +32,13 @@ async function run() {
         const result = await TestCollection.find().toArray();
         res.send(result);
     })
-    
+    app.get('/tests/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+        const result = await TestCollection.findOne(query);
+        res.send(result);
+    })
+
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
